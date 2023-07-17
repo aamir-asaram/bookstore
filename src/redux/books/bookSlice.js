31187dc0d461c63e88db0734518d6c457e7fd530
Aamir <aamirasaram2107@gmail.com>
2023-07-17 08:16:41 +0200

    Fix eslint errors

---
+++ src/redux/books/bookSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    addBook: (state, action) => {
+      return { ...state, value: state.value.push(action.payload) };
+    },
+    deleteBook: (state, action) => {
+      return { ...state, value: state.value.filter((book) => book.id !== action.payload) }
+    }
+  },
+});
+
+export const { addBook, deleteBook, } = booksSlice.actions;
+
+export default booksSlice.reducer;
